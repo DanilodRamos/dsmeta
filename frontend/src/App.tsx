608d854d4e9f05bdbfb8f499ad8e212e6a4f798d
Danilo Ramos <danilodramos@yahoo.com.br>
2022-11-17 11:55:44 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
-import NotificationButton from "./componentes/NotificationButton"
+import Header from "./componentes/Header"
+import SalesCard from "./componentes/SalesCard"
+
 
 
 function App() {
     return (
         <>
-        <h1>ola nundo</h1>
-            <NotificationButton />
-            <NotificationButton />
-            <NotificationButton />
+        <Header />
+            <main>
+                <section id="sales">
+                    <div className="dsmeta-container">
+                        <SalesCard />
+                    </div>
+                </section>
+                </main>
             </>
     )
 }
