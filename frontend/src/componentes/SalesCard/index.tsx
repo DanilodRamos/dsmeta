764055aac0fe133009824862e4c880b75ef0570a
Danilo Ramos <danilodramos@yahoo.com.br>
2022-11-18 10:47:27 -0300

    Sale listing

--- frontend/src/componentes/SalesCard/index.tsx
+++ frontend/src/componentes/SalesCard/index.tsx
@@ -4,20 +4,24 @@ import "react-datepicker/dist/react-datepicker.css";
 import './styles.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { BASE_URL } from "../../utils/request";
+import { Sale } from "../../models/sale";
 
-function SalesCard() {
+function SalesCard(): JSX.Element {
 
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
     const max = new Date();
 
-    const [minDate, setMinDate ] = useState(min);
+    const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
-    useEffect(()=> {
-        axios.get("http://localhost:8080/Sales")
-        .then(response => {
-            console.log(response.data);
-        });
+    const [sales, setSales] = useState<Sale[]>([]);
+
+    useEffect(() => {
+        axios.get('${BASE_URL}/Sales')
+            .then(response => {
+                setSales(response.data.content);
+            });
     }, []);
 
 
@@ -32,82 +36,63 @@ function SalesCard() {
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
-              </div>
-                    <div className="dsmeta-form-control-container">
+                </div>
+                <div className="dsmeta-form-control-container">
                     <DatePicker
                         selected={maxDate}
                         onChange={(date: Date) => setMaxDate(date)}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
-              </div>
-                    </div>
+                </div>
+            </div>
 
-                    <div>
-                        <table className="dsmeta-sales-table">
-                            <thead>
-                                <tr>
-                                    <th className="show992">ID</th>
-                                    <th className="show576">Data</th>
-                                    <th>Vendedor</th>
-                                    <th className="show992">Visitas</th>
-                                    <th className="show992">Vendas</th>
-                                    <th>Total</th>
-                                    <th>Notificar</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td className="show992">#341</td>
-                                    <td className="show576">08/07/2022</td>
-                                    <td>Anakin</td>
-                                    <td className="show992">15</td>
-                                    <td className="show992">11</td>
-                                    <td>R$ 55300.00</td>
+            <div>
+                <table className="dsmeta-sales-table">
+                    <thead>
+                        <tr>
+                            <th className="show992">ID</th>
+                            <th className="show576">Data</th>
+                            <th>Vendedor</th>
+                            <th className="show992">Visitas</th>
+                            <th className="show992">Vendas</th>
+                            <th>Total</th>
+                            <th>Notificar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {sales.map(sale => {
+                            return (
+                                <tr key={sale.id}>
+                                    <td className="show992">{sale.id}</td>
+                                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                    <td>{sale.sellerName}</td>
+                                    <td className="show992">{sale.visited}</td>
+                                    <td className="show992">{sale.deals}</td>
+                                    <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
                                             <Notificationbutton />
                                         </div>
                                     </td>
                                 </tr>
-                                <tr>
-                                    <td className="show992">#341</td>
-                                    <td className="show576">08/07/2022</td>
-                                    <td>Anakin</td>
-                                    <td className="show992">15</td>
-                                    <td className="show992">11</td>
-                                    <td>R$ 55300.00</td>
-                                    <td>
-                                        <div className="dsmeta-red-btn-container">
-                                        <Notificationbutton />
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="show992">#341</td>
-                                    <td className="show576">08/07/2022</td>
-                                    <td>Anakin</td>
-                                    <td className="show992">15</td>
-                                    <td className="show992">11</td>
-                                    <td>R$ 55300.00</td>
-                                    <td>
-                                        <div className="dsmeta-red-btn-container">
-                                            <Notificationbutton />  
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
+                            )
+                        })
 
-                        </table>
-                    </div>
+                        }
 
-                </div>
-           
-        )
-            
+                    </tbody>
 
-      
- 
- }
+                </table>
+            </div>
+
+        </div>
+
+    )
+
+
+
+
+}
 
 export default SalesCard;
