eef3a39f0c3daa35b867d96579f54c1411f674f6
Danilo Ramos <danilodramos@yahoo.com.br>
2022-11-18 12:17:16 -0300

    Manutantion

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dsmeta.entities;
 
 import java.time.LocalDate;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,13 +1,12 @@
 package com.devsuperior.dsmeta.services;
+
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
@@ -21,7 +20,7 @@ public class SaleService {
 		
 		LocalDate today = LocalDate.ofInstant(Instant.now(),ZoneId.systemDefault());
 		
-		LocalDate min = maxDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 		
 		return repository.findSales(min, max, pageable);
